3d914ae054ee4ae76fda1397a2f60af8f46df1b9
Malgoodev <malgoo.dev@gmail.com>
2024-05-19 22:21:46 +0700

    +login form

--- locket-clone/src/component/LoginRegister/LoginRegister.jsx
+++ locket-clone/src/component/LoginRegister/LoginRegister.jsx
@@ -6,16 +6,16 @@ const LoginRegister = () => {
         <div className="full-height">
             <div className="container">
                 <div className="login">
-                    <div className="header">
-                        <div className="header-text">Sign Up</div>
+                    <div className="Icon">
+                        {/* <div className="header-text">Sign Up</div> */}
                     </div>      
-                    <div className="inputs">
-                        <img src="" alt="" />
-                        <input type="username" />
+                    <div className="input">
+                        <img src="./Icon/User_alt.svg" alt="" className="icon"/>
+                        <input type="username" placeholder="Username"/>
                     </div>  
-                    <div className="inputs">
-                        <img src="" alt="" />
-                        <input type="password" />
+                    <div className="input">
+                        <img src="./Icon/Password.svg" alt="" className="icon"/>
+                        <input type="password" placeholder="Password"/>
                     </div>                     
                 </div>
                 <div className="forgot-password">Lost password?</div>
